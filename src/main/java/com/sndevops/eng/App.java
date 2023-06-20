75b130c764a572012a24b4777edf8108a886ac39
greydonbuckley <51009262+greydonbuckley@users.noreply.github.com>
2023-06-20 10:23:08 -0700

    GREYCORP-3: make a change

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello devops world dev branch1");
+        System.out.println( "Hello devops world dev branch");
     }
 }
